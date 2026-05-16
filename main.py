6a4bf93b2b83f3a7e14bdbc8cbf1ea96a947f1f6
Paul Yusuf <paulyusuf2000@gmail.com>
2026-05-16 03:38:39 +0100

    fix COR errors

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://farmpay-gold.vercel.app/"
+    "https://farmpay-gold.vercel.app",
+    "https://farmpay-gold.vercel.app/*"
 ]
 
 app.add_middleware(
